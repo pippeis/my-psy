529d65b11096c5007ce6e909e79ab0851c50b1d6
filippo.giardi <filippo.giardi@rcp-vision.com>
2020-04-30 16:31:47 +0200

    fixing

--- src/app/components/login-dialog/login-dialog.component.ts
+++ src/app/components/login-dialog/login-dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialogRef} from '@angular/material';
 import {AuthService} from '@app/services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-dialog-component',
@@ -10,10 +11,12 @@ import {AuthService} from '@app/services/auth.service';
 export class LoginDialogComponent implements OnInit {
   username: string;
   password: string;
+  dummyPassword = '';
 
   constructor(
     public dialogRef: MatDialogRef<LoginDialogComponent>,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {
   }
 
@@ -25,7 +28,19 @@ export class LoginDialogComponent implements OnInit {
   }
 
   onSubmit(): void {
-    this.authService.login(this.username, this.password).subscribe(() => this.dialogRef.close());
+    this.authService.login(this.username, this.password).subscribe(() => {
+      this.dialogRef.close();
+      this.router.navigate(['auth/home']);
+    });
   }
 
+  setDummyPassword(): void {
+    if (this.password && this.password.length > 0) {
+      let i = 0;
+      while (i < this.password.length) {
+        this.dummyPassword += '*';
+        i++;
+      }
+    }
+  }
 }
